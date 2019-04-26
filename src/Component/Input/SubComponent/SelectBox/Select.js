076ae52da6c951f839266348231c301fbaea4a99
Nikhil Srivastava <nikhil21.sri@gmail.com>
2019-04-26 15:58:39 +0530

    Add: RN support :heavy_plus_sign:

--- src/Component/Input/SubComponent/SelectBox/Select.js
+++ src/Component/Input/SubComponent/SelectBox/Select.js
@@ -1,6 +1,6 @@
 import React, {useMemo} from 'react';
-import {TextInput, View, Image, Animated, VirtualizedList, TouchableHighlight,TouchableOpacity} from 'react-native';
-import countryData from './Data/CountriesData'
+import {TextInput, View, Image, Animated, VirtualizedList, TouchableHighlight, TouchableOpacity} from 'react-native';
+import countryData from './Data/CountriesData';
 import * as _ from 'lodash';
 
 import CountryTile from "./CountryTile/CountryTile";
@@ -9,6 +9,8 @@ export class FlagSelect extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            cursor: 0,
+            counter:0,
             value: '',
             dropdownVisible: false,
             defaultCountry: props.defaultCountry ? props.defaultCountry : 'IN',
@@ -23,6 +25,7 @@ export class FlagSelect extends React.Component {
             countryData.map((item) => {
                 if (props.defaultCountry.toLowerCase() === item.code.toLowerCase()) {
                     this.state = {
+                        cursor: 0,
                         fullData: countryData,
                         data: countryData,
                         dialCode: item.dialCode,
@@ -82,18 +85,67 @@ export class FlagSelect extends React.Component {
         this.props.handleCode(item.dialCode, item.code)
     }
 
-    renderVirtualizedList = () => {
+    handleKeyDown(e) {
+        const {cursor, data} = this.state
+        if (e.keyCode === 38 && cursor > 0) {
+            let counter = this.state.counter
+            --counter;
+            let c = this.state.cursor;
+            --c;
+            this.setState({cursor: c, counter}, () => this.handleScroll('up'))
+        } else
+            if (e.keyCode === 40 && cursor <= data.length - 1)
+            {
+                let counter = this.state.counter
+                ++counter;
+                let c = this.state.cursor;
+                ++c;
+                this.setState({cursor: c, counter}, () => this.handleScroll('down'))
+        }
+            else {
+                if (e.key === 'Enter') {
+                    if (this.state.cursor !== 0) {
+                        if (this.props.onItemSelected) {
+                            this.props.onItemSelected(this.VTListRef.props.data[--this.state.cursor])
+                            this.setState({value: this.VTListRef.props.data[this.state.cursor][this.props.searchQuery]})
+                        }
+                    }
+                }
+            }
+    }
+
+    handleScroll = (scrollType) => {
+        if (scrollType === 'down') {
+            if (this.state.counter % Math.floor(this.state.listContainerHeight / this.props.listItemStyle.height) === 0 && this.state.cursor > 0)
+                this.VTListRef.scrollToIndex({index: this.state.cursor - 1})
+        } else if (this.state.cursor > 0)
+            this.VTListRef.scrollToIndex({index: this.state.cursor - 1})
+    }
+
+
+    renderVirtualizedList = (index,cursor) => {
         return (
             <VirtualizedList
                 data={this.state.data}
+                ref={(ref) => {
+                    this.VTListRef = ref;
+                }}
                 getItemCount={data => data.length}
+                keyboardShouldPersistTaps='handled' //might be neccesary to delete this
                 getItem={(data, index) => data[index]}
                 renderItem={({item, key}) => (
-                    <CountryTile
-                        key={key}
-                        item={item}
-                        handleMultiFunctions={this.handleMultiFunctions}
-                    />
+                    <View key={index++}
+                          onLayout={(event) => {
+                              let {height} = event.nativeEvent.layout
+                              this.setState({listItemHeight: height})
+                          }}
+                          style={{backgroundColor: cursor === index ? 'rgba(1, 140, 207, 0.46)' : null}}>
+                        <CountryTile
+                            key={key}
+                            item={item}
+                            handleMultiFunctions={this.handleMultiFunctions}
+                        />
+                    </View>
                 )
                 }
             />
@@ -101,18 +153,25 @@ export class FlagSelect extends React.Component {
     };
 
     render() {
+        let index = 0;
+        const {cursor} = this.state
         return (
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <View style={{flexDirection: 'row',backgroundColor:'transparent'}}>
+                <View style={{flexDirection: 'row', backgroundColor: 'transparent'}}>
                     <TouchableOpacity underlayColor='none' onPress={(e) => this.showDropdown(e)}
-                                        style={{width: 48, height: 20, flexDirection: 'row',}}>
+                                      style={{width: 48, height: 20, flexDirection: 'row',}}>
                         <Image source={{uri: this.state.flagIcon}}
                                style={{width: 30, height: 20, borderRadius: 6}}/>
                     </TouchableOpacity>
                 </View>
                 {this.state.dropdownVisible ?
-                    <Animated.View>
-                        <View style={{
+                    <View>
+                        <View
+                            onLayout={(event) => {
+                                let {height} = event.nativeEvent.layout
+                                this.setState({listContainerHeight: height})
+                            }}
+                            style={{
                             borderColor: '#ccc',
                             borderWidth: 1,
                             borderRadius: 7,
@@ -147,10 +206,11 @@ export class FlagSelect extends React.Component {
                                     value: '',
                                     data: this.state.fullData
                                 })}
+                                onKeyPress={(e) => this.handleKeyDown(e)}
                                 onChangeText={value => this.handleSearch(value)}
                             />
-                            {this.renderVirtualizedList()}
-                        </View></Animated.View> : null}
+                            {this.renderVirtualizedList(index,cursor)}
+                        </View></View> : null}
             </View>
 
         )
