4a62985799debb201e5090f9d3a5543da87aebb6
Nikhil Srivastava <nikhil21.sri@gmail.com>
2019-06-20 21:00:09 +0530

    implement animations while switching from email to phone && improve significant menu performance

--- src/Component/Input/SubComponent/SelectBox/CountryTile/CountryTile.js
+++ src/Component/Input/SubComponent/SelectBox/CountryTile/CountryTile.js
@@ -1,20 +1,22 @@
-import React, {memo} from 'react'
+import React,{memo} from 'react'
 import {Image, Text, TouchableWithoutFeedback, View} from "react-native";
-export function CountryTile(props) {
-    return (
-        <TouchableWithoutFeedback key={props.key} onPress={() => props.handleMultiFunctions(props.item)}>
-            <View key={props.key} style={{
-                flexDirection: 'row',
-                marginBottom: 4,
-                paddingHorizontal: 5,
-                paddingVertical: 2
-            }}>
-                <Image source={{uri: props.item.url + props.item.code.toLowerCase() + '.svg'}}
-                       style={{width: 25, height: 25}}/>
-                <Text style={{fontFamily: 'Nunito', paddingHorizontal: 10}}>{props.item.name}</Text>
-            </View>
-        </TouchableWithoutFeedback>
-    );
+
+export function CountryTile(props)  {
+        return (
+            <TouchableWithoutFeedback key={props.key} onPress={() =>props.handleMultiFunctions(props.item)}>
+                <View key={props.key} style={{
+                    flexDirection: 'row',
+                    marginBottom: 4,
+                    paddingHorizontal: 7,
+                    paddingVertical: 2
+                }}>
+                   <Image
+                       source={{uri:props.item.flag}}
+                           style={{width: 20, height: 20}}/>
+                    <Text style={{fontFamily: 'Nunito', paddingHorizontal: 10}}>{props.item.name}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        );
 }
 
-export default memo(CountryTile)
+export default memo(CountryTile);
